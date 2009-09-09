40ab5e2d7a5f8ca9ffb279f9ed297995b61aabab
John Prince <jtprince@gmail.com>
2009-09-09 00:50:04 -0600

    forgot new files

---
+++ lib/ms/msrun/mzxml.rb
@@ -0,0 +1,12 @@
+
+require 'ms/spectrum'
+require 'ms/data'
+require 'ms/data/lazy_io'
+require 'ms/msrun'
+require 'libxml'
+
+
+LibXML::XML.default_keep_blanks = false
+
+
+LibXML::XML::Reader.io(
---
+++ spec/ms/sha1_spec.rb
@@ -0,0 +1,25 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+require 'ms/sha1'
+
+class Sha1Spec < MiniTest::Spec
+  def initialize(*args)
+    @files = %w(000.v1.mzXML 020.v2.0.readw.mzXML 000.v2.1.mzXML).map do |file|
+      TESTFILES + "/opd1/#{file}"
+    end
+    super(*args)
+  end
+
+  it 'can determine a sha1 for an mzxml file' do
+    @files.each do |file|
+      (actual, recorded) = Ms::Sha1.digest_mzxml_file file
+      [actual, recorded].each {|v| assert !v.nil? }
+      puts "act and rec: "
+      p actual
+      p recorded
+      actual.must_equal recorded
+    end
+  end
+
+
+end
