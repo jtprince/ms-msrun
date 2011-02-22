56e177ba82576607bce2cfb6b918a26f0293c02d
John Prince <jtprince@gmail.com>
2011-02-21 20:29:01 -0700

    in the middle of changing index paradigm

--- Rakefile
+++ Rakefile
@@ -1,35 +1,45 @@
 require 'rubygems'
+require 'bundler'
+begin
+  Bundler.setup(:default, :development)
+rescue Bundler::BundlerError => e
+  $stderr.puts e.message
+  $stderr.puts "Run `bundle install` to install missing gems"
+  exit e.status_code
+end
 require 'rake'
 require 'jeweler'
-require 'rake/testtask'
-require 'rcov/rcovtask'
 
 NAME = "ms-msrun"
 WEBSITE_BASE = "website"
 WEBSITE_OUTPUT = WEBSITE_BASE + "/output"
 
-gemspec = Gem::Specification.new do |s|
-  s.name = NAME
-  s.authors = ["John T. Prince"]
-  s.email = "jtprince@gmail.com"
-  s.homepage = "http://jtprince.github.com/" + NAME + "/rdoc"
-  s.summary = "an mspire library for working with LC/MS runs (mzxml, mzData, mzML)"
-  s.description = 'A library for working with LC/MS runs. Part of mspire.  Has parsers for mzXML v1, 2, and 3, mzData (currently broken) and mzML (planned).  Can convert to commonly desired search output (such as mgf).  Fast random access of scans, and fast reading of the entire file.'
-  s.rubyforge_project = 'mspire'
-  s.add_dependency 'ms-core', ">= 0.0.3"
-  s.add_dependency 'nokogiri'
-  s.add_dependency 'narray'
+Jeweler::Tasks.new do |gem|
+  # gem is a Gem::Specification... see http://docs.rubygems.org/read/chapter/20 for more options
+  gem.name = "ms-msrun"
+  gem.homepage = "http://github.com/jtprince/ms-msrun"
+  gem.license = "MIT"
+  gem.summary = %Q{an mspire library for working with LC/MS runs (mzxml & mzML)}
+  gem.description = %Q{A library for working with LC/MS runs. Part of mspire.  Has parsers for mzXML v1, 2, and 3 and mzML.  Can convert to commonly desired search output (such as mgf).  Fast random access of scans, and fast reading of the entire file.}
+  gem.email = "jtprince@gmail.com"
+  gem.authors = ["John T. Prince"]
+  gem.rubyforge_project = 'mspire'
+  # I THINK THE BELOW IS OBSOLETED BY BUNDLER:
+  # Include your dependencies below. Runtime dependencies are required when using your gem,
+  # and development dependencies are only needed for development (ie running rake tasks, tests, etc)
+  #  gem.add_runtime_dependency 'jabber4r', '> 0.1'
+  #  gem.add_development_dependency 'rspec', '> 1.2.3'
 end
+Jeweler::RubygemsDotOrgTasks.new
 
-Jeweler::Tasks.new(gemspec)
-
+require 'rake/testtask'
 Rake::TestTask.new(:spec) do |spec|
-  ENV['TEST'] = ENV['SPEC'] if ENV['SPEC']  
   spec.libs << 'lib' << 'spec'
   spec.pattern = 'spec/**/*_spec.rb'
   spec.verbose = true
 end
 
+require 'rcov/rcovtask'
 Rcov::RcovTask.new do |spec|
   spec.libs << 'spec'
   spec.pattern = 'spec/**/*_spec.rb'
--- lib/ms/msrun/index.rb
+++ lib/ms/msrun/index.rb
@@ -1,43 +1,218 @@
 require 'ms/msrun'
+require 'openany'
 
 module Ms ; end
 class Ms::Msrun ; end
 
-# an index by scan number of the doublets where each doublet = [start_byte,
-# length] for the scan.  Index objects are enumerable and yield doublets.
-# Index#scan_nums gives an array of the scan numbers.
-# Index#first and Index#last return the first and the last scan, regardless of
-# the scan numbers.
+# An Ms::Msrun::Index is merely an array of doublets, where each doublet is a
+# start byte and a length.  It is always zero indexed, but it can be queried
+# by specific id's.
 #
-#     index.scan_nums  # -> [1,2,3,4]
+#     Ms::Msrun::Index.new("file.mzXML")  # returns an Ms::Msrun::Index::Mz[x]ml object
+#     index.ids # -> ["1", "2", "3" ... ]  # mzXML
+#     index.ids # -> ["controllerType=0 controllerNumber=1 scan=1", ...]  # mzML
+#     index.index_type # -> :scans | :spectra    (mzXML => :scans, mzML => :spectra)
 #     index.each do |starting_byte, length|
-#       IO.read(myfile.mzXML, length, starting_byte) # -> xml for each scan
+#       IO.read(myfile.mzXML, length, starting_byte) # -> xml for each spectrum or scan
 #     end
-#     index[0]         # -> nil
-#     index.first      # -> [<start_byte>, <length>] # for scan number 1 
-class Ms::Msrun::Index < Array
-  include Enumerable
+#     index[0]  => [start_byte, length]    of the first scan/spectrum
+#     index[-1] => [start_byte, length]    of the last scan/spectrum
+module Ms::Msrun::Index
 
-  MZXML_INDEX_TAG = 'indexOffset'
-  MZML_INDEX_TAG = 'indexListOffset'
-
-  # returns the length from the start to the first scan
-  def header_length
-    self.each {|pair| return (pair.first) }
-  end
-
-  # returns an array of the scan numbers
-  attr_reader :scan_nums
+  # an array of the id strings held in the index
+  # for mzXML, this is the scan numb
+  attr_accessor :ids
 
   # takes an mz[X]ML filename or io object
   # and returns an array of offsets and lengths for the scans
   # note that the offset 
-  def initialize(filename_or_io)
-    (ft, version) = Ms::Msrun.filetype_and_version(filename_or_io)
-    tag = case ft
-          when :mzml ; MZML_INDEX_TAG
-          when :mzxml ; MZXML_INDEX_TAG
+  def self.new(filename_or_io=nil)
+    if filename_or_io
+      (ft, version) = Ms::Msrun.filetype_and_version(filename_or_io)
+      Ms::Msrun::Index.const_get(ft.to_s.capitalize).new
+    end
+  end
+
+  # returns the length in bytes from the start to the first scan
+  def header_length
+    #self.each {|pair| return (pair.first) }
+    self[0][0]
+  end
+
+  # for each item indexed, yields the id String and the [offset, length] array
+  def id_with_each(&block)
+    ids.zip(self, &block)
+  end
+
+  # assumes io object is connected to a file so that "File.size(io.path)" is
+  # valid.  sets the 'ids' attribute to be an array of id strings and replaces
+  # self with a parallel array of doublets, where each doublet consists of a
+  # start byte and length. returns self.
+  def set_from_file_io(io)
+    (offset, length) = Ms::Msrun::Index.index_offset(io, index_offset_tag)
+    xml_st = io.seek(offset) && io.read(length)
+    index_to_array(xml_st, offset, index_ref_re)
+    (_ids, start_bytes) = id_and_start_byte_arrays(xml_st, index_ref_re)
+    doublets = start_bytes.push(last_offset).each_cons(2).map {|s,sp1| [s, sp1 - s] }
+    ids = _ids
+    self.replace(doublets)
+    self
+  end
+
+  # returns an array of each id and an array of start bytes
+  def id_and_start_byte_arrays(xml_string, index_re)
+    ids = []
+    indices = []
+    xml_string.each_line("\n") do |line|
+      if md = line.match(index_re)
+        ids << md[1]
+        indices << md[2].to_i
+      end
+    end
+    [ids, indices]
+  end
+
+  def index_offset_tag
+    self.class.const_get('INDEX_TAG')
+  end
+
+  def index_ref_re
+    self.class.const_get('INDEX_REF_RE')
+  end
+
+  # returns [index_offset, length_of_index]
+  # the filesize will be queried File.size(io.path) if not provided by
+  # filesize.
+  def self.index_offset(io, tag=MZML_INDEX_TAG, bytes_backwards=200, filesize=nil) # :nodoc:
+    filesize ||= File.size(io.path)
+    tag_re = %r{<#{tag}>([\-\d]+)</#{tag}>}
+      io.pos = filesize-1
+    io.pos = io.pos - bytes_backwards
+    _index_offset = nil
+    index_end = nil
+    io.each do |line|
+      if line =~ tag_re
+        _index_offset = $1.to_i
+        index_end = io.pos - line.size
+        break
+      end
+    end
+    if _index_offset
+      [_index_offset, index_end - _index_offset]
+    else
+      [nil,nil]
+    end
+  end
+
+  # non-destructive rewind: saves the position and returns it after the block
+  # is executed. Returns the block's reply.
+  def safe_io_rewind(io, &block)
+    start = io.pos
+    io.rewind
+    reply = block.call 
+    io.pos = start
+    reply
+  end
+
+  class Mzxml < Array
+    include Ms::Msrun::Index
+
+    INDEX_TAG = 'indexOffset'
+    INDEX_REF_RE = %r{id="(\d+)".*>(\d+)</}
+
+    # returns an array of the scan numbers as Integers
+    attr_reader :scan_nums
+
+    def initialize(filename_or_io=nil)
+      if filename_or_io
+        openany(filename_or_io) do |io|
+          set_from_file_io(io)
+        end
+      end
+    end
+  end
+
+  class Mzml < Array
+    include Ms::Msrun::Index
+
+    INDEX_TAG = 'indexListOffset'
+    INDEX_REF_RE = %r{<offset\s+idRef="([^"]+)".*>(\d+)</}
+
+    SpectrumList_re = /<spectrumList [^>]*count="(\d)+"/
+      # right now, this is flaky because a scan number may not be unique
+      Spectrum_re = /<spectrum .*?id="[^"]+".*?>/
+      Spectrum_close_re = %r{</spectrum>}
+
+    def initialize(filename_or_io=nil)
+      if filename_or_io
+        openany do |io|
+          if has_index?
+            set_from_file_io(io)
+          else
+            set_from_indexless_io(io)
           end
+        end
+      end
+    end
+
+    # reads the io and generates an Index object.
+    # assumes the file is formatted with no more than one spectrum tag per line
+    # will raise an error if this assumption is violated.
+    # returns self
+    def set_from_indexless_io(io)
+      _ids = []
+      safe_io_rewind(io) do
+        start_indices_plus_tail = []
+        # find the spectrum list tag and see how many spectra there are
+        cnt = nil
+        while line = io.gets  
+          if md = SpectrumList_re.match(line)
+            cnt = md[1].to_i
+            break
+          end
+        end
+
+        last_match_position = nil 
+        while line = io.gets
+          if md = Spectrum_re.match(line)
+            start_indices_plus_tail.push( io.pos - (md.post_match.bytesize + md.to_s.bytesize) )
+            _ids << md[1]
+            last_match_position = io.pos
+          end
+        end
+
+        io.pos = last_match_position
+        while line = io.gets
+          if md = Spectrum_close_re.match(line)
+            start_indices_plus_tail.push( 1 + io.pos -  md.post_match.bytesize )
+          end
+          prev_pos = io.pos
+        end
+        doublets = start_indices_plus_tail.each_cons(2).map {|start, fin| [start, fin - start] }
+
+        # this is a sanity check to ensure we parsed as many spectra out as we
+        # had counts for at the beginning.  If not, something is messed up!
+        raise RuntimeError, "bad index parse" if (_ids.size != cnt) || (doublets.size != cnt)
+        ids = _ids
+        self.replace(doublets)
+        self
+      end
+    end
+
+    # returns boolean based on whether the mzML file has been indexed.
+    # Implemented by looking for the indexedML tag in the first three lines of
+    # the file. returns io to wherever it was when handed in.
+    def has_index?(io)
+      safe_io_rewind(io) do
+        !!3.times.map { io.gets }.join.match(/<indexedmzML/m)
+      end
+    end
+
+  end
+end
+
+=begin
+
     fn = 
       if filename_or_io.is_a? String
         filename_or_io  # a filename
@@ -51,12 +226,16 @@ class Ms::Msrun::Index < Array
       else
         filename_or_io
       end
+    index_obj = 
+      case ft
+      when :mzml
+      when :mzxml
+      end
+    io.close if filename_or_io.is_a?(String)
+    index_obj
+  end
+
     if has_index?(io, ft)
-      (offset, length) = index_offset(io, size, tag)
-      io.pos = offset
-      xml = io.read(length)
-      io.close if filename_or_io.is_a?(String)
-      array = index_to_array(xml, offset, ft)
       self.replace(array)
     else
       # right now, the only filetype than can be without an index is mzML
@@ -64,161 +243,5 @@ class Ms::Msrun::Index < Array
     end
   end
 
-  SpectrumList_re = /<spectrumList [^>]*count="(\d)+"/o
-  # right now, this is flaky because a scan number may not be unique
-  Spectrum_re = /<spectrum .*?scan=(\d+).*?>/o
-  Spectrum_close_re = /<\/spectrum>/o
 
-  # TODO: NEED TO SORT OUT:
-  # this is sort of a hack to fit mzML onto mzXML style indexer.
-  # need to decide if mzML should use scan index as the index in (but it is a
-  # zero indexed guy) or the scan id (meaning it would not be an integer, so
-  # the object would need to be completely different).
-
-  # reads the io and generates an Index object.
-  # assumes the file is formatted with no more than one spectrum tag per line
-  # will raise an error if this assumption is violated.
-  # returns self
-  def set_from_mzml(io)
-    @scan_nums = []
-    safe_io_rewind(io) do
-      start_indices_plus_tail = []
-      # find the spectrum list tag and see how many spectra there are
-      cnt = nil
-      while line = io.gets  
-        if md = SpectrumList_re.match(line)
-          cnt = md[1].to_i
-          break
-        end
-      end
-
-      last_match_position = nil 
-      while line = io.gets
-        if md = Spectrum_re.match(line)
-          start_indices_plus_tail.push( io.pos - (md.post_match.bytesize + md.to_s.bytesize) )
-          @scan_nums << Integer(md[1])
-          last_match_position = io.pos
-        end
-      end
-
-      io.pos = last_match_position
-      while line = io.gets
-        if md = Spectrum_close_re.match(line)
-          start_indices_plus_tail.push( 1 + io.pos -  md.post_match.bytesize )
-        end
-        prev_pos = io.pos
-      end
-      start_indices_plus_tail.each_cons(2).zip(@scan_nums) do |start_fin, scan_num|
-        (start, fin) = start_fin
-        self[scan_num] = [start, fin - start]
-      end
-
-      # this is a sanity check to ensure we parsed as many spectra out as we
-      # had counts for at the beginning.  If not, we messed up.
-      raise RuntimeError, "bad index parse" if @scan_nums.size != cnt
-      self
-    end
-  end
-
-  def has_index?(io, filetype)
-    case filetype
-    when :mzxml ; true
-    when :mzml ; mzml_has_index?(io)
-    end
-  end
-
-  # returns boolean based on whether the mzML file has been indexed.
-  # Implemented by looking for the indexedML tag in the first three lines of
-  # the file. returns io to wherever it was when handed in.
-  def mzml_has_index?(io)
-    safe_io_rewind(io) do
-      !!3.times.map { io.gets }.join.match(/<indexedmzML/m)
-    end
-  end
-
-  def each(&block)
-    scan_nums.each do |scan_num|
-      block.call( self[scan_num] )
-    end
-  end
-
-  def first
-    self[scan_nums.first]
-  end
-
-  def last
-    self[scan_nums.last]
-  end
-
-  # returns [index_offset, length_of_index]
-  def index_offset(io, size, tag=MZML_INDEX_TAG, bytes_backwards=150) # :nodoc:
-    tag_re = /<#{tag}>([\-\d]+)<\/#{tag}>/
-      io.pos = size-1
-    io.pos = io.pos - bytes_backwards
-    index_offset = nil
-    index_end = nil
-    io.each do |line|
-      if line =~ tag_re
-        index_offset = $1.to_i
-        index_end = io.pos - line.size
-        break
-      end
-    end
-    if index_offset
-      [index_offset, index_end - index_offset]
-    else
-      [nil,nil]
-    end
-  end
-
-  # last_offset is used to calculate the length of the last scan object (or
-  # whatever)
-  def index_to_array(xml_string, last_offset, type=:mzml) # :nodoc:
-    indices = []
-    @scan_nums = []
-    case type
-    when :mzxml
-      xml_string.each_line("\n") do |line|
-        if line =~ /id="(\d+)".*>(\d+)</
-          @scan_nums << $1.to_i
-          indices << $2.to_i
-        end
-      end
-      #doc = Nokogiri::XML.parse(xml_string, *Ms::Msrun::Nokogiri::PARSER_ARGS)
-      #root_el = doc.root
-      #raise RuntimeError, "expecting scan index!" unless root_el['name'] == 'scan'
-      #root_el.children.each do |el|
-      #  indices << el.text.to_i
-      #  @scan_nums << el['id'].to_i
-      #end
-    when :mzml
-      xml_string.each_line("\n") do |line|
-        if line =~ /<offset idRef=".*scan=(\d+)".*>(\d+)</
-          @scan_nums << $1.to_i
-          indices << $2.to_i
-        end
-      end
-    end
-    indices << last_offset
-
-    new_indices = []
-    0.upto(indices.size-2) do |i|
-      val = indices[i]
-      next unless val
-      new_indices[@scan_nums[i]] = [indices[i], indices[i+1] - indices[i]]
-    end
-    new_indices
-  end
-
-  private
-  
-  # non-destructive rewind: saves the position and returns it after the block
-  # is executed. Returns the block's reply.
-  def safe_io_rewind(io, &block)
-    start = io.pos
-    io.rewind
-    reply = block.call 
-    io.pos = start
-    reply
-  end
-end
+=end
